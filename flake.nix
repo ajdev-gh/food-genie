381936633e22f3ff363caf32520b6c6c60de1c00
Ajai Dev <289577993+ajdev-gh@users.noreply.github.com>
2026-06-05 19:43:52 +0530

    chore(flake): add nix-community binary cache
    Configure nixConfig extra-substituters and trusted public keys to
    enable pre-built binaries from nix-community.

--- flake.nix
+++ flake.nix
@@ -10,6 +10,12 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
